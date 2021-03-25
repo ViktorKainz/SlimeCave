904a52038a6601edfb691dca4e0959634aaad486
Florian Angerer <florian.angerer.2001@gmail.com>
2021-03-25 13:19:09 +0100

    added move script and idle to move animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveHorizontal;
+    private float moveVertical;
+    public float speed;
+    public Animator animator;
+    
+    
+    public Rigidbody2D player;
+    private Vector2 movement;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        moveVertical = Input.GetAxis("Vertical") * speed;
+        
+        movement = new Vector2(moveHorizontal, moveVertical);
+
+        player.velocity = movement * speed;
+        animator.SetFloat("Speed", Mathf.Abs(player.velocity.x + player.velocity.y));
+    }   
+}
